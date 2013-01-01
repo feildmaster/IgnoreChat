2d268ef35abfb066d0b13d76ee94c4b4f102388a
feildmaster <admin@feildmaster.com>
2013-01-01 04:52:07 -0600

    Now with javadocs!
    Next step: Move to EventLibrary

--- src/main/java/com/feildmaster/event/EmoteEvent.java
+++ src/main/java/com/feildmaster/event/EmoteEvent.java
@@ -4,26 +4,59 @@ import java.util.Collection;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.HandlerList;
 
+/**
+ * An event called on the "Emote" command (/me).<br />
+ * The event stores a <b>sender</b>, a <b>message</b>, and <b>recipients</b>.
+ *
+ * @author feildmaster
+ */
 public final class EmoteEvent extends MessageEvent {
     private static final HandlerList handlers = new HandlerList();
 
     private final CommandSender sender;
 
+    /**
+     * Creates a new EmoteEvent with the provided sender and message.
+     * <p />
+     * The event created will have no recipients.
+     *
+     * @param sender the sender of the Emote
+     * @param message the original (full) message
+     */
     public EmoteEvent(final CommandSender sender, final String message) {
         super(message);
         this.sender = sender;
     }
 
+    /**
+     * Crates a new event with the provided sender, message, and recipients.
+     *
+     * @param sender the sender of the Emote
+     * @param message the original (full) message
+     * @param recipients the recipients of the message
+     */
     public EmoteEvent(final CommandSender sender, final String message, CommandSender... recipients) {
         super(message, recipients);
         this.sender = sender;
     }
 
+    /**
+     * Crates a new event with the provided sender, message, and recipients.
+     *
+     * @param sender the sender of the Emote
+     * @param message the original (full) message
+     * @param recipients the recipients of the message
+     */
     public EmoteEvent(final CommandSender sender, final String message, Collection<CommandSender> recipients) {
         super(message, recipients);
         this.sender = sender;
     }
 
+    /**
+     * Gets the sender of the emote.
+     *
+     * @return the sender
+     */
     public CommandSender getSender() {
         return sender;
     }
--- src/main/java/com/feildmaster/event/MessageEvent.java
+++ src/main/java/com/feildmaster/event/MessageEvent.java
@@ -7,32 +7,77 @@ import java.util.Set;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.Event;
 
+/**
+ * An abstract event designed to be a common handler for generic "MessageEvents."
+ * <p />
+ * A message event has <b>recipients</b> and a <b>message</b>.
+ * <p />
+ * Implementations may override the method {@link #setMessage(java.lang.String)}
+ * to make it a no-operation function.
+ *
+ * @author feildmaster
+ */
 public abstract class MessageEvent extends Event {
     private final Set<CommandSender> recipients = new HashSet<CommandSender>();
     private String message;
 
+    /**
+     * Crates a MessageEvent with the provided message, and no recipients.
+     *
+     * @param message the original message
+     */
     public MessageEvent(String message) {
         this.message = message;
     }
 
+    /**
+     * Crates a MessageEvent with the provided message and recipients.
+     *
+     * @param message the original message
+     * @param recipients the recipients of the message
+     */
     public MessageEvent(String message, CommandSender... recipients) {
         this(message);
         Collections.addAll(getRecipients(), recipients);
     }
 
+    /**
+     * Crates a MessageEvent with the provided message and recipients.
+     *
+     * @param message the original message
+     * @param recipients the recipients of the message
+     * @throws NullPointerException if recipients is null
+     */
     public MessageEvent(String message, Collection<CommandSender> recipients) {
         this(message);
         getRecipients().addAll(recipients);
     }
 
+    /**
+     * Gets the message to be sent.
+     *
+     * @return the message to be sent
+     */
     public String getMessage() {
         return message;
     }
 
+    /**
+     * Sets the message to be sent.
+     *
+     * @param message new message to be sent
+     */
     public void setMessage(String message) {
         this.message = message;
     }
 
+    /**
+     * Gets a set of {@link CommandSender's} the message will be sent to.
+     * <p />
+     * <b>This set is not guaranteed to be mutable and may throw an {@link UnsupportedOperationException} if an ImmutableSet is provided.</b>
+     *
+     * @return all command senders the message will be sent to
+     */
     public final Set<CommandSender> getRecipients() {
         return recipients;
     }
